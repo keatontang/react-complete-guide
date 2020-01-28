8e09225217082122d9cb4a766a7dac85e4bbd4d4
Keaton Tang <keaton.tang@gmail.com>
2020-01-28 17:29:11 +1300

    completed up to module 33

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Ola, me llamo Keaton</h1>
-    //   </div>
-    // );
-      return React.createElement('div', null,"h1", "Ola, me llamo Keaton");
+    return (
+      <div className="App">
+        <h1>Ola, me llamo Keaton</h1>
+          <p>Dis work</p>
+          <Person />
+      </div>
+    );
+      // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "It works"));
   }
 }
 
