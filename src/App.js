79db89a40f8bcbfc757c98eaa007b4d7a3de7e98
Keaton Tang <keaton.tang@gmail.com>
2020-01-30 10:15:37 +1300

    module 39

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Keaton', age: 19},
+            {name: 'kebin', age: 20},
+            {name: 'karson', age: 30}
+        ]
+    }
   render() {
     return (
       <div className="App">
         <h1>Ola, me llamo Keaton</h1>
           <p>Dis work</p>
-          <Person />
-          <Person />
-          <Person />
+          <button>Switch name</button>
+          <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+          <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}> my hobby is ball</Person>
+          <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
       </div>
     );
       // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "It works"));
