5448a4e12fa134440350407eefa11c1e0cf8b64e
Keaton Tang <keaton.tang@gmail.com>
2020-01-30 19:14:28 +1300

    module 43

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ class App extends Component {
             {name: 'Keaton', age: 19},
             {name: 'kebin', age: 20},
             {name: 'karson', age: 30}
-        ]
+        ],
+        otherState: "yees"
     }
+
+    switchNameHandler = () => {
+        // console.log("yee");
+        this.setState({persons: [
+                {name: 'Kkrn', age: 19},
+                {name: 'kebin', age: 20},
+                {name: 'karson', age: 40}
+            ]})
+    }
+
   render() {
     return (
       <div className="App">
         <h1>Ola, me llamo Keaton</h1>
           <p>Dis work</p>
-          <button>Switch name</button>
+          <button onClick = {this.switchNameHandler}>Switch name</button>
           <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
           <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}> my hobby is ball</Person>
           <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
